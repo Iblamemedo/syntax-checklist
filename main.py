364b2e36c67b7633260d7e0d37510ce8603422bc
Mohamed Hassan <mail.mhmd@icloud.com>
2026-07-03 12:48:49 +0300

    Task 1 Complete

---
+++ main.py
@@ -0,0 +1,6 @@
+def Task1 ():
+    my_list = ['Cairo', 'Alexandria', 'Giza', 'Luxor', 'Aswan']
+    print(my_list[2])  # This will print 'Giza' since list indexing starts at 0
+    return 0
+
+Task1()
